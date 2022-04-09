72910f05bf022b9aaca5a6646a5662919b455a82
Aiden <mi_duo@live.com>
2022-04-09 20:13:47 +0800

    feat: bak

--- config/config_test.go
+++ config/config_test.go
@@ -1,21 +1,28 @@
 package config
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 var data = `
+---
 a: Easy!
 b:
   c: 2
   d: [3, 4]
+---
+name: abc
+age: 10
+---
 `
 
 type D struct {
@@ -24,10 +31,29 @@ type D struct {
 		RenamedC int   `yaml:"c"`
 		D        []int `yaml:",flow"`
 	}
+	Name string `yaml:"name"`
+	Age  int    `yaml:"age"`
+}
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+func TestYaml(t *testing.T) {
+	d2 := D{}
+	decoder := yaml.NewDecoder(strings.NewReader(data))
+	err := decoder.Decode(&d2)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- d2:\n%v\n\n", d2)
 }
 
 func TestYaml1(t *testing.T) {
 	d := D{}
+	s := Student{}
+	_ = s
 
 	err := yaml.Unmarshal([]byte(data), &d)
 	if err != nil {
@@ -54,6 +80,7 @@ func TestYaml1(t *testing.T) {
 		log.Fatalf("error: %v", err)
 	}
 	fmt.Printf("--- m dump:\n%s\n\n", string(b))
+
 }
 
 func TestYaml2(t *testing.T) {
@@ -80,4 +107,33 @@ func TestYaml2(t *testing.T) {
 	err = json.Unmarshal([]byte(c.Body), tb)
 	assert.Nil(t, err)
 	t.Log(tb)
+}
+
+type (
+	T struct {
+		A string
+		B string
+	}
+)
+
+func Parse(source []byte) (err error) {
+	dec := yaml.NewDecoder(bytes.NewReader(source))
+
+	var doc T
+	for dec.Decode(&doc) == nil {
+		fmt.Println(doc)
+	}
+
+	return
+}
+
+func TestYaml3(t *testing.T) {
+	s := `
+---
+a: val a
+---
+b: val b
+---
+`
+	Parse([]byte(s))
 }
