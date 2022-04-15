49d4ca5ef4a3cf016fa78611ec06196eb60c6933
Aiden <mi_duo@live.com>
2022-04-15 22:13:34 +0800

    test: grpc

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/jhump/protoreflect v1.12.0
 	github.com/stretchr/testify v1.7.1
+	google.golang.org/grpc v1.45.0
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
@@ -13,8 +14,9 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
-	google.golang.org/grpc v1.45.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- internal/test/proto_desc_test.go
+++ internal/test/proto_desc_test.go
@@ -1,10 +1,15 @@
 package test
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
+	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/desc/protoparse"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
 )
 
 /*
@@ -28,8 +33,11 @@ func TestDynamicProto(t *testing.T) {
 	for _, fileDesc := range fileDescriptors {
 		t.Logf("===============\nfileDesc: %v", fileDesc)
 		t.Logf("package: %s, gopackage: %s", fileDesc.GetPackage(), fileDesc.AsFileDescriptorProto().GetOptions().GetGoPackage())
+		t.Logf("Metadata: %s", fileDesc.GetName()) // protos/api/student_api.proto
 		for _, msgDesc := range fileDesc.GetMessageTypes() {
 			t.Logf("msgDesc: %v", msgDesc)
+			msgDesc.AsProto().ProtoMessage()
+			proto.Marshal(msgDesc.AsDescriptorProto())
 		}
 		for _, servDesc := range fileDesc.GetServices() {
 			t.Logf("service info: %v", servDesc)
@@ -38,6 +46,84 @@ func TestDynamicProto(t *testing.T) {
 				t.Logf("methods %+v", methodInfo)
 			}
 		}
+		CreateServiceDesc(fileDesc)
+	}
+
+}
+
+func CreateServiceDesc(fileDesc *desc.FileDescriptor) {
+	for _, servDescriptor := range fileDesc.GetServices() {
+		serviceDesc := grpc.ServiceDesc{
+			ServiceName: servDescriptor.GetName(),
+			Metadata:    fileDesc.GetName(),
+		}
+		for _, methodDescriptor := range servDescriptor.GetMethods() {
+			isServerStream := methodDescriptor.IsServerStreaming()
+			isClientStream := methodDescriptor.IsClientStreaming()
+			if isServerStream || isClientStream {
+				streamDesc := grpc.StreamDesc{
+					StreamName:    methodDescriptor.GetName(),
+					Handler:       nil,
+					ServerStreams: isServerStream,
+					ClientStreams: isClientStream,
+				}
+				serviceDesc.Streams = append(serviceDesc.Streams, streamDesc)
+			} else {
+				methodDesc := grpc.MethodDesc{
+					MethodName: methodDescriptor.GetName(),
+					Handler:    createUnaryServerHandler(serviceDesc, methodDescriptor.GetName()),
+				}
+				serviceDesc.Methods = append(serviceDesc.Methods, methodDesc)
+			}
+		}
+
+		//Methods: []grpc.MethodDesc{
+		//	{
+		//		MethodName: "NewStudent",
+		//		Handler:    _StudentSrv_NewStudent_Handler,
+		//	},
+		//	{
+		//		MethodName: "StudentByID",
+		//		Handler:    _StudentSrv_StudentByID_Handler,
+		//	},
+		//},
+		//Streams: []grpc.StreamDesc{
+		//	{
+		//		StreamName:    "AllStudent",
+		//		Handler:       _StudentSrv_AllStudent_Handler,
+		//		ServerStreams: true,
+		//	},
+		//	{
+		//		StreamName:    "StudentInfo",
+		//		Handler:       _StudentSrv_StudentInfo_Handler,
+		//		ServerStreams: true,
+		//		ClientStreams: true,
+		//	},
+		//},
+	}
+}
+
+func createUnaryServerHandler(serviceDesc grpc.ServiceDesc, methodName string) func(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	return func(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+		fmt.Println(serviceDesc.ServiceName)
+		fmt.Println(methodName)
+		fmt.Println(srv)
+		//in := new(QueryStudent)
+		//if err := dec(in); err != nil {
+		//	return nil, err
+		//}
+		//if interceptor == nil {
+		//	return srv.(StudentSrvServer).StudentByID(ctx, in)
+		//}
+		//info := &grpc.UnaryServerInfo{
+		//	Server:     srv,
+		//	FullMethod: "/api.StudentSrv/StudentByID",
+		//}
+		//handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		//	return srv.(StudentSrvServer).StudentByID(ctx, req.(*QueryStudent))
+		//}
+		//return interceptor(ctx, in, info, handler)
+		return nil, nil
 	}
 
 }
