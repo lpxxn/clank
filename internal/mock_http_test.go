3ca91cdab9c2120d17f6c10c5622f7efd75552a2
Aiden <mi_duo@live.com>
2022-05-13 09:45:13 +0800

    feat: http serv

--- internal/mock_http_test.go
+++ internal/mock_http_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"log"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -8,4 +9,8 @@ import (
 
 func TestParam(t *testing.T) {
 	engion := gin.New()
+	engion.Any("/restaurant/:id/order/:orderNo", func(c *gin.Context) {
+		log.Println(c.Params)
+
+	})
 }
