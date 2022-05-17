8566cd7298bf181673d3a956ffb8d083a677d52a
Aiden <mi_duo@live.com>
2022-05-17 09:13:28 +0800

    feat: log

--- internal/clanklog/logger_test.go
+++ internal/clanklog/logger_test.go
@@ -1,12 +1,13 @@
 package clanklog
 
 import (
+	"errors"
 	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
-	os.Setenv("CLANK_LOG_LEVEL", "info")
+	//os.Setenv("CLANK_LOG_LEVEL", "info")
 	//os.Setenv("CLANK_LOG_FORMATTER", "json")
 	NewLogger()
 	os.Exit(m.Run())
@@ -15,4 +16,7 @@ func TestMain(m *testing.M) {
 func TestLogger1(t *testing.T) {
 	Info("a", "b")
 	Infof("a %s", "b")
+	Error("err1", "err2")
+	Errorf("hello err: %+v", errors.New("hahahha"))
+	Fatalf("wtf: %+v", errors.New("surprise"))
 }
