b0ef176227ac8466401c72f0e11e27ded6c47a04
Aiden <mi_duo@live.com>
2022-06-03 11:51:11 +0800

    feat: http request

---
+++ internal/grpc_request.go
@@ -0,0 +1,37 @@
+package internal
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func NewHttpRequestWithHeader(ctx context.Context, url string, method string, body []byte, header map[string]string) ([]byte, error) {
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range header {
+		req.Header.Add(k, v)
+	}
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	req.WithContext(ctx)
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return readBody(resp.Body)
+}
+
+func readBody(body io.ReadCloser) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, body); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
